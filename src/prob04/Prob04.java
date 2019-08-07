c81bc641e4ab6dc8e1d8176d8aac0263e4276484
BIT <BIT@DESKTOP-Q04GE57>
2019-08-07 16:13:54 +0900

    4번 완료

--- src/prob04/Prob04.java
+++ src/prob04/Prob04.java
@@ -2,19 +2,27 @@ package prob04;
 public class Prob04 {
 
 	public static void main(String[] args) {
-		char[] c1 = reverse( "Hello World" );
-		printCharArray( c1 );
+		char[] c1 = reverse("Hello World");
+		printCharArray(c1);
 		
-		char[] c2 = reverse( "Java Programming!" );
-		printCharArray( c2 );
+		char[] c2 = reverse("Java Programming!");
+		printCharArray(c2);
 	}
 	
 	public static char[] reverse(String str) {
-		/* 코드를 완성합니다 */
-		return null;
+		char[] c = new char[str.length()];
+		
+		for(int i = 0; i < str.length(); i++) {
+			c[i] = str.charAt(str.length() - i - 1);
+		}
+		
+		return c;
 	}
 
 	public static void printCharArray(char[] array){
-		/* 코드를 완성합니다 */
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+		}
+		System.out.println("");
 	}
 }
