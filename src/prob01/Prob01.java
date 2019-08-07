9e58be9c95361a1ff32e3616512643cd571730e3
BIT <BIT@DESKTOP-Q04GE57>
2019-08-07 15:56:30 +0900

    1번 완료

--- src/prob01/Prob01.java
+++ src/prob01/Prob01.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		Scanner sc = new Scanner( System.in  );
 		
 		System.out.print( "금액: " );
-		int money = scanner.nextInt();
+		int money = sc.nextInt();
+		int[] money_arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
+		int[] count_arr = new int[10];
+		int start = 0;
 		
-		scanner.close();
+		while(money > 0) {
+			count_arr[start] = money / money_arr[start];
+			money %= money_arr[start];
+			start++;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(money_arr[i] + "원 : " + count_arr[i] + "개");
+		}
+		
+		sc.close();
  	}
 }
