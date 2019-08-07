3e8fa7d31cd9c26e6df3144e09a2a08418a293af
BIT <BIT@DESKTOP-Q04GE57>
2019-08-07 16:35:51 +0900

    5번 완료

--- src/prob05/Prob05.java
+++ src/prob05/Prob05.java
@@ -1,15 +1,57 @@
 package prob05;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-
-
+		Scanner sc = new Scanner( System.in );
 		
-		scanner.close();
+		Random rnd = new Random();
+		int max = 100;
+		int min = 1;
+		int ans = rnd.nextInt(max) + 1;
+		int count = 1;
+		
+		System.out.println("수를 결정하였습니다. 맞추어보세요");
+		
+		while(true) {
+			System.out.println(min + "-" + max);
+			System.out.print(count + ">>");
+			
+			int input = sc.nextInt();
+			
+			if(input == ans) {
+				System.out.println("맞았습니다.");
+				System.out.print("다시하시겠습니까(y/n) : ");
+				
+				String s = sc.next();
+				
+				if(s.equals("y")) {
+					count = 1;
+					min = 1;
+					max = 100;
+					ans = rnd.nextInt(max) + 1;
+					System.out.println("수를 결정하였습니다. 맞추어보세요");
+					continue;
+				} else if(s.equals("n")) {
+					System.out.println("종료.");
+					break;
+				}
+			} else {
+				if(input > ans) {
+					max = Math.min(max, input);
+					System.out.println("더낮게");
+				} else if(input < ans) {
+					min = Math.max(min, input);
+					System.out.println("더높게");
+				}
+				count++;
+			}
+		}
+		
+		sc.close();
 	}
 
 }
